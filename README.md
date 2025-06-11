0bf97758ea6fac632f4165a96663e32e8f633866
Izaac <30945012+IzaacLuo@users.noreply.github.com>
2025-06-11 11:37:04 +0800

    Setup Flutter project with localization

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # MBTIRPG
+
+A Flutter-based MBTI-themed role playing game.
+
+## Build and Run
+
+1. [Install Flutter](https://docs.flutter.dev/get-started/install) and set up your development environment.
+2. Fetch dependencies:
+   ```sh
+   flutter pub get
+   ```
+3. Generate localization files:
+   ```sh
+   flutter gen-l10n
+   ```
+4. Run the app on a connected device or emulator:
+   ```sh
+   flutter run
+   ```
+5. To build release binaries:
+   - Android APK: `flutter build apk`
+   - iOS app: `flutter build ios`
+
+The project includes localization scaffolding for English and Chinese. Edit files in `lib/l10n/` to add or update translations.
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MBTIRPG',
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'),
+        Locale('zh'),
+      ],
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.title),
+      ),
+      body: Center(
+        child: Text(AppLocalizations.of(context)!.helloWorld),
+      ),
+    );
+  }
+}
